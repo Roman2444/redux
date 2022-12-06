547f0a6f40eb1db716b88b0980c5f5eb970e8456
Roman Belykh <106916877+Roman2444@users.noreply.github.com>
2022-12-06 11:08:30 +0300

    state.value corrected to ctate.counter, added useSelector

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,28 +1,33 @@
 import { connect } from "react-redux";
 import * as actions from '../actions';
+import {useSelector} from 'react-redux';
+
 // import { bindActionCreators } from "redux";
 
 const Counter = ({counter, dec, inc, rnd}) => {
 
+    const counter = useSelector(state => state.counter)
+
     return (
         <div className="jumbotron">
             <h1>{counter}</h1>
-            <button onClick={dec} className="btn btn-primary">DEC</button>
+            {/* <button onClick={dec} className="btn btn-primary">DEC</button>
             <button onClick={inc} className="btn btn-primary">INC</button>
-            <button onClick={rnd} className="btn btn-primary">RND</button>
+            <button onClick={rnd} className="btn btn-primary">RND</button> */}
         </div>
 
     )
 }
 
-const mapStateToProps = (state) => {
-    return {
-        counter: state.value
-    }
-}
+// const mapStateToProps = (state) => {
+//     return {
+//         counter: state.value
+//     }
+// }
 
 // const mapDispatchToProps = (dispatch) => {
 //     return  bindActionCreators(actions, dispatch)
 // }
     
-export default connect(mapStateToProps, actions)(Counter);
+// export default connect(mapStateToProps, actions)(Counter);
+export default Counter;
